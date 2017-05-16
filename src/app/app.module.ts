b469dfe239ca1b4f4fecc079dd856058ee0d173f
Santiago Schwarz <sschwarzpazos@gmail.com>
2017-05-16 15:50:06 +0200

    feat(app.module): imported buscar component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { BuscarComponent } from './components/buscar/buscar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BuscarComponent
   ],
   imports: [
     BrowserModule,
