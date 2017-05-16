bb7e74b2b229f74b25401e17b2b8a1a3637a34cc
Santiago Schwarz <sschwarzpazos@gmail.com>
2017-05-16 15:37:03 +0200

    feat(app.routes): setting default route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,7 +3,7 @@ import { HomeComponent } from './components/home/home.component';
 
 const APP_ROUTES: Routes = [
   { path: 'home', component: HomeComponent },
-  { path: '**', pathMatch: 'full', redirectTo: '' }
+  { path: '**', pathMatch: 'full', redirectTo: 'home' }
 ];
 
 export const APP_ROUTING = RouterModule.forRoot(APP_ROUTES);
