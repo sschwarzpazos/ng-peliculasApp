a1ff8fe07ae8d8e08c1bb2a4207aad4ac96b92b5
Santiago Schwarz <sschwarzpazos@gmail.com>
2017-05-16 13:15:02 +0200

    feat(app.module): import JsonpModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { HttpModule } from '@angular/http';
+import { HttpModule, JsonpModule } from '@angular/http';
 
 //import services
 import { PeliculasService } from './services/peliculas.service';
@@ -14,7 +14,8 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    JsonpModule
   ],
   providers: [
     PeliculasService
