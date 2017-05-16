bb2e1441c8ce0662c8a8635e2efd7710c42c4894
Santiago Schwarz <sschwarzpazos@gmail.com>
2017-05-16 15:33:23 +0200

    feat(app.routes): added home route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,8 @@
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
 
 const APP_ROUTES: Routes = [
-  { path: 'routePath', component: Component },
+  { path: 'home', component: HomeComponent },
   { path: '**', pathMatch: 'full', redirectTo: '' }
 ];
 
