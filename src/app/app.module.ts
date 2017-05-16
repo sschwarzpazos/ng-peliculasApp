dac24814b5fd7de9b1e621d9cf3faece43e1fd3b
Santiago Schwarz <sschwarzpazos@gmail.com>
2017-05-16 15:27:13 +0200

    feat(app.module): imported Routes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,8 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
+//import Routes
+import { APP_ROUTING } from './app.routes';
 import { AppComponent } from './app.component';
 
 @NgModule({
@@ -12,7 +14,8 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    APP_ROUTING
   ],
   providers: [],
   bootstrap: [AppComponent]
