4a0d5d19f85b3a1c0a01052031265ab1e77f5f85
Santiago Schwarz <sschwarzpazos@gmail.com>
2017-05-16 13:13:23 +0200

    feat(ts): import Peliculas Service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,8 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
+//import services
+import { PeliculasService } from './services/peliculas.service';
 import { AppComponent } from './app.component';
 
 @NgModule({
@@ -14,7 +16,9 @@ import { AppComponent } from './app.component';
     FormsModule,
     HttpModule
   ],
-  providers: [],
+  providers: [
+    PeliculasService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
