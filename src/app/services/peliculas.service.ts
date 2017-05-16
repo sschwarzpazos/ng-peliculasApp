9462773f7b264ee961b33a6358d288655e4cad69
Santiago Schwarz <sschwarzpazos@gmail.com>
2017-05-16 13:23:55 +0200

    feat(ts): set apikey property

--- src/app/services/peliculas.service.ts
+++ src/app/services/peliculas.service.ts
@@ -4,9 +4,9 @@ import 'rxjs/Rx'; // Map
 
 @Injectable()
 export class PeliculasService {
-  private apikey:string = "";
+  private apikey:string = "535a7ab8990d9bca6de83d0b9618cd8d";
   private urlMoviedb:string = "https://api.themoviedb.org/3";
-  
+
   constructor() { }
 
 }
