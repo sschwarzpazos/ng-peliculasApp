f6dd3908b2f3333f6a295184a7e0f8033cb4dbf9
Santiago Schwarz <sschwarzpazos@gmail.com>
2017-05-16 13:27:52 +0200

    feat(ts): created getPopulares method

--- src/app/services/peliculas.service.ts
+++ src/app/services/peliculas.service.ts
@@ -19,4 +19,12 @@ export class PeliculasService {
                 .map( res=> res.json());
   }
 
+  buscarPelicula( texto:string ){
+
+    let url = `${ this.urlMoviedb }/search/movie?query=${ texto }&sort_by=popularity.desc&api_key=${ this.apikey }&language=es&callback=JSONP_CALLBACK`;
+
+    return this.jsonp.get( url )
+                .map( res=> res.json());
+  }
+
 }
