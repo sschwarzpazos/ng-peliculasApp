bed5f8d023e5dd87d7f469cf29f019a18390576c
Santiago Schwarz <sschwarzpazos@gmail.com>
2017-05-17 16:11:21 +0200

    feat(*): titulo property created

--- src/app/components/home/galeria.component.ts
+++ src/app/components/home/galeria.component.ts
@@ -7,6 +7,7 @@ import { Component, OnInit, Input } from '@angular/core';
 })
 export class GaleriaComponent implements OnInit {
   @Input('peliculas') peliculas; // peliculas es un alias
+  @Input('titulo') titulo; // titulo es un alias
 
   constructor() { }
 
