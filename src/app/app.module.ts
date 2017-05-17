3697b91272ea695f78b0ecf21da86a2bcdf87161
Santiago Schwarz <sschwarzpazos@gmail.com>
2017-05-17 12:27:02 +0200

    feat(app.module): imported peliculasImagenPipe

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { PeliculaImagenPipe } from './pipes/pelicula-imagen.pipe';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PeliculaImagenPipe
   ],
   imports: [
     BrowserModule,
