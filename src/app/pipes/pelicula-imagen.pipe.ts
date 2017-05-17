fd3b0f1be9febd13b9bb80c4a044fd72084f745e
Santiago Schwarz <sschwarzpazos@gmail.com>
2017-05-17 12:37:06 +0200

    feat(ts): Implemented PeliculaImagenPipe

--- src/app/pipes/pelicula-imagen.pipe.ts
+++ src/app/pipes/pelicula-imagen.pipe.ts
@@ -5,8 +5,16 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class PeliculaImagenPipe implements PipeTransform {
 
-  transform(value: any, args?: any): any {
-    return null;
+  transform( pelicula: any ): any {
+    if ( pelicula.backdrop_path ) {
+      return pelicula.backdrop_path;
+    } else {
+      if (pelicula.poster_path) {
+        return pelicula.poster_path;
+      } else {
+        return "assets/img/no-image.png";
+      }
+    }
   }
 
 }
