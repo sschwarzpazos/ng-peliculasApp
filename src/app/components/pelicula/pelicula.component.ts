7447a786a4f38a3da997f7831cae71d1fbf418e0
Santiago Schwarz <sschwarzpazos@gmail.com>
2017-05-18 13:02:26 +0200

    feat(ts): ActivatedRoute from '@angular/router' imported

--- src/app/components/pelicula/pelicula.component.ts
+++ src/app/components/pelicula/pelicula.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-pelicula',
@@ -7,7 +8,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PeliculaComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private route:ActivatedRoute
+  ) {
+    this.route.params.subscribe(
+      params => {
+        console.log( params )
+      }
+    );
+  }
 
   ngOnInit() {
   }
