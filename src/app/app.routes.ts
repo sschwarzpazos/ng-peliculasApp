59f5d9d3325243d9a4c066ea3a25355753b6f28e
Santiago Schwarz <sschwarzpazos@gmail.com>
2017-05-18 16:06:27 +0200

    fix(ts): 'buscar' route lost

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,6 +5,7 @@ import { PeliculaComponent } from './components/pelicula/pelicula.component';
 
 const APP_ROUTES: Routes = [
   { path: 'home', component: HomeComponent },
+  { path: 'buscar', component: BuscarComponent },
   { path: 'buscar/:texto', component: BuscarComponent },
   { path: 'pelicula/:id/:pag', component: PeliculaComponent },
   { path: '**', pathMatch: 'full', redirectTo: 'home' }
