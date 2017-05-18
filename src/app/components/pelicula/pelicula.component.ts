79d5b729b40416860291ff6942a7ad2a58f10c20
Santiago Schwarz <sschwarzpazos@gmail.com>
2017-05-18 16:23:40 +0200

    feat(*): 'busqueda' value storaged

--- src/app/components/pelicula/pelicula.component.ts
+++ src/app/components/pelicula/pelicula.component.ts
@@ -12,6 +12,7 @@ export class PeliculaComponent implements OnInit {
   private pelicula:any;
   private regresarA:string = "";
   private busqueda:string = "";
+
   constructor(
     private ps:PeliculasService,
     private route:ActivatedRoute
@@ -19,6 +20,10 @@ export class PeliculaComponent implements OnInit {
     this.route.params.subscribe(
       params => {
         this.regresarA = params[ 'pag' ];
+        if ( params[ 'busqueda' ] ) {
+          this.busqueda = params[ 'busqueda' ];
+        }
+
         this.ps
           .getPelicula( params[ 'id' ] )
           .subscribe( _pelicula => {
